ce25c3dff4ee7e9b7884404950077b4019b3f006
ae_King <aclengen@gmail.com>
2022-06-23 16:54:58 -0400

    don't hate me for my disorganization bude

--- plugins/controllers/client/menuController.js
+++ plugins/controllers/client/menuController.js
@@ -4,24 +4,52 @@ const { logger, fastifyResponse } = require("../../utilities");
 /**
  * This is an example controller with a basic callset.
  */
- class menuController {
-    static clientMenuLocale = async (request = null, reply = null) => {
-       let playerAccount = account.get(await fastifyResponse.getSessionID(request));
-       if(playerAccount) {
-            if(playerAccount.lang != (null || undefined || "") && playerAccount.lang != request.params['language'])
-            {
+class menuController {
+
+    static getAccountLanguage = async (request, sessionID) => {
+        const playerAccount = account.get(await fastifyResponse.getSessionID(request));
+        if (playerAccount) {
+            if (playerAccount.lang != (null || undefined || "") && playerAccount.lang != request.params['language']) {
                 playerAccount.lang = request.params['language'];
             }
+        }
+        return playerAccount.lang;
+    }
 
-            logger.logDebug(database.locales[playerAccount.lang].menu);
-
-            return await fastifyResponse.zlibJsonReply
+    static clientMenuLocale = async (request = null, reply = null) => {
+        const lang = await menuController.getAccountLanguage(request);
+        logger.logDebug(database.locales[lang].menu);
+        return fastifyResponse.zlibJsonReply
             (
                 reply,
-                fastifyResponse.applyBody(database.locales[playerAccount.lang].menu)
+                fastifyResponse.applyBody(database.locales[lang].menu)
             )
-       }
     }
+
+
+    /**
+     * i am still trying to figure out how you're doing 
+     * things so i didn't wanna mess anything up
+     */
+    static clientGlobalLocale = async (request = null, reply = null) => {
+        const lang = await menuController.getAccountLanguage(request);
+        return fastifyResponse.zlibJsonReply
+            (
+                reply,
+                fastifyResponse.applyBody(database.locales.global[lang])
+            )
+    }
+
+    static clientGetLanguages = async (request = null, reply = null) => {
+        return fastifyResponse.zlibJsonReply
+            (
+                reply,
+                fastifyResponse.applyBody(database.locales.languages)
+            )
+    }
+
+
+
 }
 
 module.exports.menuController = menuController;
--- plugins/routes/client.js
+++ plugins/routes/client.js
@@ -19,7 +19,7 @@ module.exports = async function gameRoutes(app, opts) {
 
     app.post(`/client/game/start`, async (request, reply) => {
         return await gameController.clientGameStart(request, reply);
-    }); 
+    });
 
     app.post(`/client/game/version/validate`, async (request, reply) => {
         return await gameController.clientGameVersionValidate(request, reply);
@@ -30,4 +30,13 @@ module.exports = async function gameRoutes(app, opts) {
     app.post(`/client/menu/locale/:language`, async (request, reply) => {
         return await menuController.clientMenuLocale(request, reply);
     });
+
+    app.post(`/client/locale/:language`, async (request, reply) => {
+        return await menuController.clientGlobalLocale(request, reply);
+    })
+
+    app.post(`/client/languages`, async (request, reply) => {
+        return await menuController.clientGetLanguages(request, reply);
+    });
+    
 }
