558c33391aa265f280b35d7e8fe4bf5ed3235c00
Nehaxfr <benji.vandeginste@gmail.com>
2022-07-02 21:36:30 +0200

    hideout loading routes

--- plugins/routes/client.js
+++ plugins/routes/client.js
@@ -157,7 +157,20 @@ module.exports = async function gameRoutes(app, _opts) {
     // hideout routes
 
     app.post(`/client/hideout/areas`, async (request, reply) => {
-        await ClientController.clientHideoutArea(request, reply);
+        await ClientController.clientHideoutAreas(request, reply);
+    });
+
+    app.post(`/client/hideout/production/recipes`, async (request, reply) => {
+        await ClientController.clientHideoutProductionRecipes(request, reply);
+    });
+
+    
+    app.post(`/client/hideout/production/scavcase/recipes`, async (request, reply) => {
+        await ClientController.clientHideoutProductionScavcaseRecipes(request, reply);
+    });
+    
+    app.post(`/client/hideout/settings`, async (request, reply) => {
+        await ClientController.clientHideoutSettings(request, reply);
     });
 
 }
