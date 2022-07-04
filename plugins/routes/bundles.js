75b22a687499b233c0c88fd1039ef5e664fe4d4c
ae_King <aclengen@gmail.com>
2022-07-04 09:44:28 -0400

    quests loaded in db

--- plugins/routes/bundles.js
+++ plugins/routes/bundles.js
@@ -1,10 +1,12 @@
 /**
  * GET:/getBundleList
  */
-const { BundlesController } = require("../controllers/client");
+const { BundlesController } = require("../controllers");
+const { logger } = require("../utilities");
 
 
 module.exports = async function singleplayerBundles(app, opts) {
+  logger.logWarning("Bundles not implemented yet")
   app.get(`/getBundleList`, async (_request, _reply) => {
     await BundlesController.getBundles();
   })
