fb500361ff8a8e663fca6559b57a539dc9ede829
Nehaxfr <nehaxfr@proton.me>
2022-07-08 17:27:09 +0200

    added notifier model

---
+++ plugins/models/Notification.js
@@ -0,0 +1,25 @@
+const { BaseModel } = require("./BaseModel");
+
+class Notification extends BaseModel {
+    constructor(id) {
+        super(id);
+
+    }
+
+    async createNewNotification(message) {
+        const newNotification = {
+            type: "new_message",
+            eventId: message._id,
+            data: {
+                dialogId: message.uid,
+                message
+            }
+        };
+        if(!this.queue) {
+            this.queue = [];
+        }
+        this.queue.push(newNotification);
+    }
+}
+
+module.exports.Notification = Notification;
