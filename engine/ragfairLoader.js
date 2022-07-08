edef51a8b6cb40d6edef9fef5320ae1b7e5f9135
ae_King <aclengen@gmail.com>
2022-07-08 10:19:54 -0400

    `ragfairLoader` created

---
+++ engine/ragfairLoader.js
@@ -0,0 +1,12 @@
+const { Ragfair } = require("../plugins/models");
+
+class RagfairLoader {
+    static async loadRagfairDatabase() {
+        await this.loadRagfair();
+    }
+
+
+    static async loadRagfair() {
+        
+    }
+}
