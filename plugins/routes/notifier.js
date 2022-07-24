d0969f6cf70bbad1b426e611a2b730478f5a5e6d
Nehaxfr <nehaxfr@proton.me>
2022-07-24 21:39:30 +0200

    ws handler is hit, connection not made (rep 200)

--- plugins/routes/notifier.js
+++ plugins/routes/notifier.js
@@ -12,39 +12,21 @@ module.exports = async function notifierRoutes(app, opt) {
         );
     });
 
-    app.route({
-        method: "GET",
-        url: '/push/notifier/get/',
-        handler: async (request, reply) => {
-            logger.logError("NOTIFIER GET HIT");
-            return FastifyResponse.zlibJsonReply(
-                reply,
-                FastifyResponse.applyBody([])
-            );
-        },
-        wsHandler: (connection, req) => {
-            logger.logError("NOTIFIER GET WS HIT");
-            connection.socket.on('message', message => {
-                connection.socket.send('Hello Fastify WebSockets');
-            });
-        }
+    app.get('/push/notifier/get/', { websocket: true }, async (connection, req) => {
+        logger.logError("NOTIFIER GET WS HIT");
+        connection.socket.on('message', message => {
+            connection.socket.send('Hello Fastify WebSockets');
+        });
     })
 
-    app.route({
-        method: "GET",
-        url: '/push/notifier/getwebsocket/',
-        handler: async (request, reply) => {
-            logger.logError("NOTIFIER getwebsocket GET HIT");
-            return FastifyResponse.zlibJsonReply(
-                reply,
-                FastifyResponse.applyBody([])
-            );
-        },
-        wsHandler: (connection, req) => {
-            logger.logError("NOTIFIER getwebsocket GET WS HIT");
+    app.get('/push/notifier/getwebsocket/', { websocket: true }, async (connection, req) => {
+        logger.logError("NOTIFIER getwebsocket GET WS HIT");
+        connection.socket.on('connection', (connection) => {
+            logger.logError("CONNECTION")
             connection.socket.on('message', message => {
                 connection.socket.send('Hello Fastify WebSockets');
             });
-        }
+        })
+        
     })
 };
