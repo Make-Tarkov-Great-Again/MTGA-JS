0b3a5c2f21d3788e8cdcd14ffeb8a56691788b3b
Nehaxfr <benji.vandeginste@gmail.com>
2022-07-26 17:52:47 +0200

    mental breakdown on websocket

--- plugins/routes/notifier.js
+++ plugins/routes/notifier.js
@@ -12,57 +12,27 @@ module.exports = async function notifierRoutes(app, opt) {
         );
     });
 
-    app.route({
-        method: "POST",
-        url:'/:lastID',
-        handler: async (request, reply) => {
-            logger.logError("NOTIFIER HIT");
-            return FastifyResponse.zlibJsonReply(
-                reply,
-                FastifyResponse.applyBody("NOTIFY")
-            );
-        },
-       //wsHandler: (connection, req) => {
-       //    logger.logError("NOTIFIER WS HIT");
-       //    connection.socket.on('message', message => {
-       //        connection.socket.send('Hello Fastify WebSockets');
-       //    });
-       //}
+    app.post("/:lastID", async (request, reply) => {
+        logger.logError("NOTIFIER HIT");
+        return FastifyResponse.zlibJsonReply(
+            reply,
+            FastifyResponse.applyBody("NOTIFY")
+        );
     });
 
-    app.route({
-        method: "POST",
-        url: '/push/notifier/get/',
-        handler: async (request, reply) => {
-            logger.logError("NOTIFIER GET HIT");
+    app.post("/push/notifier/get/:sessionID", async (request, reply) => {
+        logger.logError("NOTIFIER GET HIT");
             return FastifyResponse.zlibJsonReply(
                 reply,
                 FastifyResponse.applyBody("ok")
-            );
-        },
-        //wsHandler: (connection, req) => {
-        //    logger.logError("NOTIFIER GET WS HIT");
-        //    connection.socket.on('message', message => {
-        //        connection.socket.send('Hello Fastify WebSockets');
-        //    });
-        //}
-    })
+        );
+    });
 
-    app.route({
-        method: "POST",
-        url: '/push/notifier/getwebsocket/',
-        handler: async (request, reply) => {
-            logger.logError("NOTIFIER getwebsocket GET HIT");
+    app.post("/push/notifier/getwebsocket/:sessionID", async (request, reply) => {
+        logger.logError("NOTIFIER getwebsocket GET HIT");
             return FastifyResponse.zlibJsonReply(
                 reply,
                 FastifyResponse.applyBody("ok")
-            );
-        },
-        //wsHandler: (connection, req) => {
-        //    logger.logError("NOTIFIER getwebsocket GET WS HIT");
-        //    connection.socket.on('message', message => {
-        //        connection.socket.send('Hello Fastify WebSockets');
-        //    });
-        //}
-    })
+        );
+    });
 };
