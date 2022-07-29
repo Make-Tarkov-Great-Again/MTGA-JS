ce575dbc29835cd80232df2e285bfb5896a4ffe7
IAMBUDE <93278696+IAMBUDE@users.noreply.github.com>
2022-07-29 20:49:59 +0200

    Websocket stuff - Still a lot to do

--- plugins/routes/notifier.js
+++ plugins/routes/notifier.js
@@ -5,8 +5,14 @@ const { logger, FastifyResponse, writeFile } = require("../utilities");
 module.exports = async function notifierRoutes(app, opt) {
 
     // Client Notifier Routes //
+    app.post("/client/WebSocketAddress", async (request, reply) => {
+        return FastifyResponse.zlibReply(
+            reply,
+            FastifyResponse.getWebSocketUrl()
+        );
+    });
+
     app.post("/client/notifier/channel/create", async (request, reply) => {
-        console.log(request.body)
         const sessionID = await FastifyResponse.getSessionID(request)
         return FastifyResponse.zlibJsonReply(
             reply,
@@ -14,15 +20,7 @@ module.exports = async function notifierRoutes(app, opt) {
         );
     });
 
-    app.post("/:lastID", async (request, reply) => {
-        logger.logError("NOTIFIER HIT");
-        return FastifyResponse.zlibJsonReply(
-            reply,
-            FastifyResponse.applyBody("NOTIFY")
-        );
-    });
-
-    app.post("/push/notifier/get/:sessionID", async (request, reply) => {
+    app.post("/:sessionID", async (request, reply) => {
         logger.logError("NOTIFIER GET HIT");
             return FastifyResponse.zlibJsonReply(
                 reply,
@@ -30,19 +28,14 @@ module.exports = async function notifierRoutes(app, opt) {
         );
     });
 
-    app.get("/push/notifier/getwebsocket/:sessionID", { websocket: true }, async (connection, request, reply) => {
+    app.get("/socket", { websocket: true }, async (connection, request, reply) => {
         logger.logError("NOTIFIER getwebsocket GET HIT");
         connection.socket.on('message', message => {
-            // message.toString() === 'hi from client'
-            connection.socket.send('NOTIFIER getwebsocket GET HIT')
+            connection.socket.send('NOTIFIER message HIT')
         })
-    });
 
-    app.post("/getNotifier", async (request, reply) => {
-        logger.logError("NOTIFIER getwebsocket GET HIT");
-            return FastifyResponse.zlibJsonReply(
-                reply,
-                FastifyResponse.applyBody("ok")
-        );
+        connection.socket.on('upgrade', message => {
+            connection.socket.send('NOTIFIER upgrade HIT')
+        })
     });
 };
