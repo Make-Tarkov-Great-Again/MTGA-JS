b875912cd622583533a51c432aa2ea69eea6d367
SlejmUr <52343819+SlejmUr@users.noreply.github.com>
2022-08-01 15:20:35 +0200

    Update sit.js
    removed cus asked

--- plugins/routes/sit.js
+++ plugins/routes/sit.js
@@ -1,14 +1,6 @@
  const { logger, FastifyResponse } = require("../utilities");
  
  module.exports = async function sitRoutes(app, _opts) {
-  /*
-   app.get(`/getBundleList`, async (_request, reply) => {
-     await FastifyResponse.zlibJsonReply(
-        reply,
-        []
-    );
-   })*/
-
    //[Slejm] Post or Get? probably POST
    app.post(`/client/sit-validator`, async (_request, reply) => {
     await FastifyResponse.zlibJsonReply(
@@ -16,12 +8,4 @@
         true
     );
   })
-/*
-  app.post(`/client/WebSocketAddress`, async (_request, reply) => {
-    await FastifyResponse.zlibJsonReply(
-        reply,
-        FastifyResponse.getWebSocketUrl()
-    );
-  })
-*/
 }
