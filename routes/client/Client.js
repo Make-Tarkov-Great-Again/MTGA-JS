c9486bcdc639d8a2649948be3fe98b92d1208088
Nehaxfr <nehaxfr@proton.me>
2022-08-04 13:10:55 +0200

    added bannedItems to database core, added function to check if item banned, updated scavcase to avoid bannedItem in rewards todo: handle stack (randomize or always full ?)

--- routes/client/Client.js
+++ routes/client/Client.js
@@ -40,12 +40,12 @@ module.exports = async function clientRoutes(app, _opts) {
     });
 
     app.post(`/client/server/list`, async (request, reply) => {
-        const { database } = require("../app");
+        const { database } = require("../../app");
         return FastifyResponse.zlibJsonReply(
             reply,
             FastifyResponse.applyBody([{ ip: database.core.serverConfig.ip, port: database.core.serverConfig.port }]));
     });
-    
+
     app.post(`/client/menu/locale/:language`, async (request, reply) => {
         await MenuController.clientMenuLocale(request, reply);
     });
