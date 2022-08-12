de27794958a2e2eb769d6c6965a6feeb60891b6d
ae_King <aclengen@gmail.com>
2022-08-12 18:56:36 -0400

    a lot of inRaid routes implemented

--- routes/client/Raid.js
+++ routes/client/Raid.js
@@ -1,24 +1,60 @@
-const { default: stringify } = require("fast-safe-stringify");
-const { logger } = require("../../utilities");
+const { logger, stringify, FastifyResponse } = require("../../utilities");
+const { Profile } = require("../../lib/models/Profile");
 
 module.exports = async function raidRoutes(app, _opts) {
 
-    app.post(`/client/raid/person/killed/showMessage`, async (request, reply) => {
+    app.post(`/client/raid/person/killed/showMessage`, async (request, _reply) => {
         console.log(request.body);
-        return logger.logDebug("Raid person killed show message not implemented yet");
+        const { database } = require("../../app");
+        const sessionID = await FastifyResponse.getSessionID(request)
+
+        const fence = database.core.gameplay.fence;
+        const killScavChange = fence.killingScavsFenceLevelChange;
+        const killPmcChange = fence.killingPMCsFenceLevelChange;
+
+        // if the killer is the player
+        if (request.body.killedByAID === sessionID) {
+            const account = await Profile.get(sessionID);
+            const profile = await account.getPmc();
+
+            if (request.body.diedFaction === "Savage" || request.body.diedFaction === "Scav")
+                profile.TradersInfo["579dc571d53a0658a154fbec"].standing += killScavChange;
+            else if (request.body.diedFaction === "Usec" || request.body.diedFaction === "Bear")
+                profile.TradersInfo["579dc571d53a0658a154fbec"].standing += killPmcChange;
+
+            await profile.saveCharacter();
+        }
+        return stringify({});
     });
 
-    app.post(`/client/raid/createFriendlyAI`, async (request, reply) => {
+
+    app.post(`/client/raid/person/killed`, async (request, _reply) => {
         console.log(request.body);
-        return logger.logDebug("Raid create friendly AI not implemented yet");
+        const { database } = require("../../app");
+        const showMessage = database.core.gameplay.inRaid.showDeathMessage;
+
+        if (showMessage) {
+            return stringify(showMessage)
+        } else return stringify(false);
     });
 
-    app.post(`/client/raid/bots/getNewProfile`, async (request, reply) => {
+
+    app.post(`/client/raid/createFriendlyAI`, async (request, _reply) => {
         console.log(request.body);
-        return logger.logDebug("Raid bots get new profile not implemented yet");
+        const { database } = require("../../app");
+        const createFriendlyAI = database.core.gameplay.inRaid.createFriendlyAI;
+
+        if (createFriendlyAI) {
+            return stringify(createFriendlyAI)
+        } else return stringify(false);
     });
 
-    app.post(`/client/raid/person/lootingContainer`, async (request, reply) => {
+    app.post(`/client/raid/bots/getNewProfile`, async (request, _reply) => {
+        console.log(request.body);
+        return stringify({});
+    });
+
+    app.post(`/client/raid/person/lootingContainer`, async (request, _reply) => {
         console.log(request.body);
         return stringify("")
     });
