3e4536a88585166ef3c6990fb3b6205f56e2c880
Nehaxfr <benji.vandeginste@gmail.com>
2022-08-17 22:48:01 +0200

    added RaidController, after raid: - save stats, - save health (including energy & hydration), - save level and xp, - save in raid examined items

--- routes/raid.js
+++ routes/raid.js
@@ -1,11 +1,12 @@
 const { logger, FastifyResponse, writeFile, stringify } = require("../utilities");
+const { RaidController } = require("../lib/controllers/RaidController");
 
 module.exports = async function serverRoutes(app, _opts) {
 
     app.post(`/raid/profile/save`, async (request, reply) => {
 
         writeFile("./saveInformation.json", stringify(request.body));
-        logger.logDebug("/raid/profile/save not implemented yet");
+        await RaidController.raidProfileSave(request, reply);
         return FastifyResponse.zlibJsonReply(
             reply,
             null
