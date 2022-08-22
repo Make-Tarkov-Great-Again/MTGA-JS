be3780cd930dbfd2d41c6f7d06591ddef1c6f8ce
Nehaxfr <benji.vandeginste@gmail.com>
2022-08-22 21:00:18 +0200

    moved hideout function from ClientController to HideoutController

--- routes/client/Hideout.js
+++ routes/client/Hideout.js
@@ -1,21 +1,21 @@
-const { ClientController } = require("../../lib/controllers");
+const { HideoutController } = require("../../lib/controllers");
 
 module.exports = async function hideoutRoutes(app, _opts) {
 
     app.post(`/client/hideout/areas`, async (request, reply) => {
-        await ClientController.clientHideoutAreas(request, reply);
+        await HideoutController.clientHideoutAreas(request, reply);
     });
 
     app.post(`/client/hideout/production/recipes`, async (request, reply) => {
-        await ClientController.clientHideoutProductionRecipes(request, reply);
+        await HideoutController.clientHideoutProductionRecipes(request, reply);
     });
 
     app.post(`/client/hideout/production/scavcase/recipes`, async (request, reply) => {
-        await ClientController.clientHideoutProductionScavcaseRecipes(request, reply);
+        await HideoutController.clientHideoutProductionScavcaseRecipes(request, reply);
     });
 
     app.post(`/client/hideout/settings`, async (request, reply) => {
-        await ClientController.clientHideoutSettings(request, reply);
+        await HideoutController.clientHideoutSettings(request, reply);
     });
-    
+
 };
