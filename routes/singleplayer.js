e0b9e196e6e59d4fa2e3e980f77e0d8a6be758f4
ae_King <aclengen@gmail.com>
2022-08-24 07:42:53 -0400

    non destructive changes i have made

--- routes/singleplayer.js
+++ routes/singleplayer.js
@@ -24,6 +24,7 @@ module.exports = async function singleplayerRoutes(app, _opts) {
 
     app.get(`/singleplayer/settings/raid/menu`, async (_request, reply) => {
         const { database: { core: { gameplay: { defaultRaidSettings } } } } = require("../app");
+
         return FastifyResponse.zlibJsonReply(
             reply,
             FastifyResponse.applyBody(defaultRaidSettings)
