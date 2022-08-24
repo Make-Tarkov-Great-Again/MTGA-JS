90c54fb26abf4b8cffa7c04ca8f3bf50c709bc9d
ae_King <aclengen@gmail.com>
2022-08-24 13:36:52 -0400

    slight change to `insurance/items/list/cost`

--- routes/client/Insurance.js
+++ routes/client/Insurance.js
@@ -9,24 +9,24 @@ module.exports = async function insuranceRoutes(app, _opts) {
 
     app.post(`/client/insurance/items/list/cost`, async (request, reply) => {
         const playerProfile = await Profile.get(await FastifyResponse.getSessionID(request));
-        const inventory = await playerProfile.getInventoryItems()
+        const inventory = await playerProfile.getInventoryItems();
 
         const output = {};
 
-        for (const trader of request.body.traders) {
-            const base = await Trader.getTraderBase(trader);
-            const loyalty = await playerProfile.getLoyalty(trader, base);
-            const traderLoyalty = base.loyaltyLevels[loyalty];
+        for (const traderID of request.body.traders) {
+            const base = await Trader.getTraderBase(traderID);
+            const loyaltyLevel = await playerProfile.getLoyalty(traderID, base);
+            const { insurance_price_coef } = base.loyaltyLevels[loyaltyLevel];
 
             const traderItems = {};
 
             for (const key of request.body.items) {
-                const item = inventory.find(i => i._id === key)
-                const coef = traderLoyalty.insurance_price_coef / 100
+                const item = inventory.find(i => i._id === key);
+                const coef = insurance_price_coef / 100
                 const insurance = ~~((coef * await Item.getItemPrice(item._tpl)) * coef);
                 traderItems[item._tpl] = insurance;
             }
-            output[trader] = traderItems
+            output[traderID] = traderItems
         }
 
 
