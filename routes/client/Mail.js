12f885d9e8a801f71d4011ad0bedcf72990b45a6
ae_King <aclengen@gmail.com>
2022-08-25 16:47:48 -0400

    mail functions, cleanup

--- routes/client/Mail.js
+++ routes/client/Mail.js
@@ -1,14 +1,28 @@
 const { ClientController } = require("../../lib/controllers");
-const { FastifyResponse, logger } = require("../../utilities");
-
 
 module.exports = async function ragfairRoutes(app, _opts) {
 
     app.post(`/client/mail/dialog/list`, async (request, reply) => {
-        logger.logConsole(`[mail/dialog/list  (NOT IMPLEMENTED)] : ${request.body}`);
-        return FastifyResponse.zlibJsonReply(
-            reply,
-            FastifyResponse.applyBody(await ClientController.clientMailDialogList(request, reply))
-        );
+        await ClientController.clientMailDialogList(request, reply)
+    });
+
+    app.post(`/client/mail/dialog/view`, async (request, reply) => {
+        await ClientController.clientMailDialogView(request, reply)
+    });
+
+    app.post(`/client/mail/dialog/remove`, async (request, reply) => {
+        await ClientController.clientMailDialogRemove(request, reply);
+    });
+
+    app.post(`/client/mail/dialog/pin`, async (request, reply) => {
+        await ClientController.clientMailDialogPin(request, reply);
+    });
+
+    app.post(`/client/mail/dialog/unpin`, async (request, reply) => {
+        await ClientController.clientMailDialogUnpin(request, reply);
+    });
+
+    app.post(`/client/mail/dialog/read`, async (request, reply) => {
+        await ClientController.clientMailDialogRead(request, reply);
     });
 };
