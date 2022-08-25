93ef120e3bb899601df083e2d4c390f5efb53c5b
ae_King <aclengen@gmail.com>
2022-08-24 21:35:47 -0400

    insuring item works, insurance incomplete still

--- routes/client/Insurance.js
+++ routes/client/Insurance.js
@@ -1,37 +1,8 @@
-//const { InsuranceController } = require("../../lib/controllers")
-
-const { Trader } = require("../../lib/models/Trader")
-const { Profile } = require("../../lib/models/Profile")
-const { logger, FastifyResponse, isCategory } = require("../../utilities")
-const { Item } = require("../../lib/models/Item")
+const { InsuranceController } = require("../../lib/controllers")
 
 module.exports = async function insuranceRoutes(app, _opts) {
 
     app.post(`/client/insurance/items/list/cost`, async (request, reply) => {
-        const playerProfile = await Profile.get(await FastifyResponse.getSessionID(request));
-        const inventory = await playerProfile.getInventoryItems();
-
-        const output = {};
-
-        for (const traderID of request.body.traders) {
-            const base = await Trader.getTraderBase(traderID);
-            const loyaltyLevel = await playerProfile.getLoyalty(traderID, base);
-            const { insurance_price_coef } = base.loyaltyLevels[loyaltyLevel];
-
-            const traderItems = {};
-
-            for (const key of request.body.items) {
-                const item = inventory.find(i => i._id === key);
-                const coef = insurance_price_coef / 100
-                const insurance = ~~((coef * await Item.getItemPrice(item._tpl)) * coef);
-                traderItems[item._tpl] = insurance;
-            }
-            output[traderID] = traderItems
-        }
-
-
-        return FastifyResponse.zlibJsonReply(
-            reply,
-            FastifyResponse.applyBody(output));
+        await InsuranceController.insuranceCost(request, reply);
     })
 }
