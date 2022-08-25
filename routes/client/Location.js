e835134ffeff605b33c1791d3581a8aa27e7bd69
ae_King <aclengen@gmail.com>
2022-08-25 16:47:48 -0400

    mail functions, cleanup

--- routes/client/Location.js
+++ routes/client/Location.js
@@ -1,6 +1,5 @@
 const { LocationController } = require("../../lib/controllers");
 
-
 module.exports = async function locationRoutes(app, _opts) {
 
     app.post(`/client/location/getLocalloot`, async (request, reply) => {
