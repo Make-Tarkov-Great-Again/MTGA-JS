712dc3c4f1ed27d85306d5b0f37bc9eb2da122ac
ae_King <aclengen@gmail.com>
2022-08-27 19:03:15 -0400

    Squashed commit of the following:
    commit 057d7db37c1aa436cc8e3b76f611c1e0dc32cdff
    Author: ae_King <aclengen@gmail.com>
    Date:   Sat Aug 27 19:02:26 2022 -0400
    
        notifications working in entirety
    
    commit ed5504e1e2188b17ea57601608e44afbc82dc557
    Author: ae_King <aclengen@gmail.com>
    Date:   Sat Aug 27 18:24:00 2022 -0400
    
        notifications working
    
    commit afcc9b1d59adcebaed733d3835ca2ac72a7e6377
    Author: ae_King <aclengen@gmail.com>
    Date:   Sat Aug 27 15:23:47 2022 -0400
    
        push
    
    commit 72daff9910f2dec07a68fbb97c2e683fcc7f325a
    Author: ae_King <aclengen@gmail.com>
    Date:   Fri Aug 26 17:36:58 2022 -0400
    
        more shit that probably doesn't work
    
    commit 72d149c6b41067a3784a16bcaaf71b5aecb4b142
    Author: ae_King <aclengen@gmail.com>
    Date:   Fri Aug 26 16:47:05 2022 -0400
    
        god is cruel
    
    commit d94f9a02da2c594161cd7bdd7fa8bf946aa691e0
    Author: ae_King <aclengen@gmail.com>
    Date:   Fri Aug 26 16:12:07 2022 -0400
    
        notifications won't send
    
    commit 4acb39ba0a165bc44741ba31cb3d257ac5309566
    Author: ae_King <aclengen@gmail.com>
    Date:   Fri Aug 26 09:14:14 2022 -0400
    
        broken `(but close)`

--- routes/notifier.js
+++ routes/notifier.js
@@ -1,11 +1,12 @@
 const { logger, FastifyResponse } = require("../utilities");
+const { NotificationController } = require("../lib/controllers/NotificationController");
 
 
 module.exports = async function notifierRoutes(app, _opt) {
 
     // Client Notifier Routes //
-    app.post("/client/WebSocketAddress", async (_request, reply) => {
-        const sessionID = await FastifyResponse.getSessionID(_request);
+    app.post("/client/WebSocketAddress", async (request, reply) => {
+        const sessionID = await FastifyResponse.getSessionID(request)
         return FastifyResponse.zlibReply(
             reply,
             FastifyResponse.getWebSocketUrl(sessionID)
@@ -20,16 +21,8 @@ module.exports = async function notifierRoutes(app, _opt) {
         );
     });
 
-    app.get("/socket/:sessionID", { websocket: true }, async (connection, request, _reply) => {
-        logger.logDebug(request.params.sessionID);
-        logger.logError("NOTIFIER getwebsocket GET HIT");
-        connection.socket.on('message', _message => {
-            connection.socket.send('NOTIFIER message HIT');
-        });
-
-        connection.socket.on('upgrade', _message => {
-            connection.socket.send('NOTIFIER upgrade HIT');
-        });
+    app.get("/socket/:sessionID", { websocket: true }, async function wsHandler (connection, request) {
+        await NotificationController.onUpgrade(connection, request)
     });
 
 };
