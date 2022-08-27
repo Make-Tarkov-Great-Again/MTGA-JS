1d0f25b0a4b9fdd19b439ff8591d980bc46e60b2
IAMBUDE <93278696+IAMBUDE@users.noreply.github.com>
2022-08-27 21:40:29 +0200

    Changes to Websockets to accommodate the sessionID

--- routes/notifier.js
+++ routes/notifier.js
@@ -5,9 +5,10 @@ module.exports = async function notifierRoutes(app, _opt) {
 
     // Client Notifier Routes //
     app.post("/client/WebSocketAddress", async (_request, reply) => {
+        const sessionID = await FastifyResponse.getSessionID(_request);
         return FastifyResponse.zlibReply(
             reply,
-            FastifyResponse.getWebSocketUrl()
+            FastifyResponse.getWebSocketUrl(sessionID)
         );
     });
 
@@ -19,15 +20,8 @@ module.exports = async function notifierRoutes(app, _opt) {
         );
     });
 
-    app.post("/:sessionID", async (_request, reply) => {
-        logger.logError("NOTIFIER GET HIT");
-        return FastifyResponse.zlibJsonReply(
-            reply,
-            FastifyResponse.applyBody("ok")
-        );
-    });
-
-    app.get("/socket", { websocket: true }, async (connection, _request, _reply) => {
+    app.get("/socket/:sessionID", { websocket: true }, async (connection, request, _reply) => {
+        logger.logDebug(request.params.sessionID);
         logger.logError("NOTIFIER getwebsocket GET HIT");
         connection.socket.on('message', _message => {
             connection.socket.send('NOTIFIER message HIT');
