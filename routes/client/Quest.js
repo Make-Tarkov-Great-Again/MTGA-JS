06b2f02f1a9b570aab8cd5f927061b87d9b611dc
ae_King <aclengen@gmail.com>
2022-09-03 07:30:36 -0400

    TaskerController added and quests updated

--- routes/client/Quest.js
+++ routes/client/Quest.js
@@ -1,4 +1,5 @@
-const { ClientController } = require("../../lib/controllers");
+const { ClientController, TaskerController } = require("../../lib/controllers");
+const { Profile } = require('../../lib/models/Profile');
 const { logger, FastifyResponse } = require("../../utilities");
 
 module.exports = async function questRoutes(app, _opts) {
@@ -8,6 +9,12 @@ module.exports = async function questRoutes(app, _opts) {
     });
 
     app.post(`/client/repeatalbeQuests/activityPeriods`, async (request, reply) => {
+        const sessionID = await FastifyResponse.getSessionID(request);
+
+        // last route to be called, will call tasks here to have them created;
+        await TaskerController.runTasks(sessionID);
+
+
         logger.warn("[repeatalbeQuests/activityPeriods (NOT IMPLEMENTED)]");
         return FastifyResponse.zlibJsonReply(
             reply,
