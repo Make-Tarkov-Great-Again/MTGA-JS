db0f62fdf1c0897a178e44acae98f42672eaa773
Nehaxfr <nehaxfr@proton.me>
2022-09-13 22:53:32 +0200

    updated logger to write logs to /serverLogs/{timestamp}.txt

--- utilities/logger.js
+++ utilities/logger.js
@@ -1,5 +1,5 @@
-const { utilFormat } = require("./utility");
-
+const { utilFormat, getCurrentTimestamp} = require("./utility");
+const fs = require("fs");
 class Logger {
   constructor() {
     this.consoleColor = {
@@ -11,7 +11,7 @@ class Logger {
         blue: "\x1b[34m",
         magenta: "\x1b[35m",
         cyan: "\x1b[36m",
-        white: "\x1b[37m",
+        white: "\x1b[37m"
       },
       back: {
         black: "\x1b[40m",
@@ -21,46 +21,51 @@ class Logger {
         blue: "\x1b[44m",
         magenta: "\x1b[45m",
         cyan: "\x1b[46m",
-        white: "\x1b[47m",
+        white: "\x1b[47m"
       },
       reset: "\x1b[0m"
+    };
+    this.logFilePath = `${this.getLogsFolderPath()}${this.getFileName()}`;
+
+    console.log(this.logFilePath)
+    if (!fs.existsSync(this.getLogsFolderPath())) {
+      fs.mkdirSync(this.getLogsFolderPath());
     }
-    // this will be created once first log occurs
-    this.logFileStream = undefined;
+    //fs.writeFileSync(this.logFilePath, "START LOG");
   }
 
   /** Returns the Filename for Logs
    * @returns string - Logs file name
    */
-  getFileName = () => `${LoggerUtils.getIsoDateString(true)}.log`;
+  getFileName() {
+    return `${getCurrentTimestamp()}.txt`;
+  } 
 
   /** Returns the path to the Logs folder with / at the end
-   * @param {boolean} useRelative 
-   * @returns 
+   * @param {boolean} useRelative
+   * @returns
    */
-  getLogsFolderPath(useRelative = true) {
-    if (useRelative) {
-      return "Local/Logs/";
-    }
-    return process.cwd() + "/Local/Logs/";
+  getLogsFolderPath() {
+    return process.cwd() + "/serverLogs/";
   }
 
   /**
-   * 
    * @param {string} type ("front", "back")
    * @param {string} color ("black", "red", "green", "yellow", "blue", "magenta", "cyan", "white")
    * @returns 
    */
   getConsoleColor(type = "front", color = "white") {
     const colorTag = this.consoleColor[type][color];
-    if (colorTag == undefined)
+    if (!colorTag)
       return "";
     return colorTag;
   }
+
   console(data) {
     // allow to change this shit in 1 place and everywhere it will be changed
     console.log(data);
   }
+
   /** Write log in console and into the file
    * @param {string} type ("LogData", "[Error]") - will be written first in log
    * @param {any} data - object or string that will be written in logs
@@ -74,46 +79,48 @@ class Logger {
 
     const logString = `${frontColor}${backColor}${type}${this.consoleColor.reset} ${time} `;
     const fileString = `${type}${time}`;
-
-    try {
-      if (this.logFileStream == undefined) {
-        // somehow this shit is crashing...
-        //this.LogFileStream = JET.util.fileIO.createFileWriteStream( this.getLogsFolderPath() + this.getFileName() );
-      }
-    } catch { }
+    const logFileStream = fs.createWriteStream(this.logFilePath, {flags: 'a+'});
     if (typeof data == "string") {
       this.console(logString + data);
-      if (this.logFileStream != undefined) {
-        this.logFileStream.write(utilFormat(fileString + data + "\n"));
-      }
+      logFileStream.once('open', function(fd) {
+        logFileStream.write(utilFormat(fileString + data + "\n"));
+        logFileStream.end();
+      });
     } else {
       this.console(logString);
       this.console(data);
-      if (this.logFileStream != undefined) {
-        this.logFileStream.write(utilFormat(fileString));
-        this.logFileStream.write(utilFormat(data));
-        this.logFileStream.write(utilFormat("\n"));
-      }
+      logFileStream.once('open', function(fd) {
+        logFileStream.write(utilFormat(fileString));
+        logFileStream.write(utilFormat(data));
+        logFileStream.write(utilFormat("\n"));
+        logFileStream.end();
+      });
     }
+    logFileStream.on("end", function() {
+      logFileStream.end();
+    });
   }
 
   info(text) {
     this.log("[INFO]", text, "white", "blue");
   }
+
   success(text) {
     this.log("[SUCCESS]", text, "white", "green");
   }
+
   warn(text) {
     this.log("[WARNING]", text, "white", "yellow");
   }
+
   error(text) {
     this.log("[ERROR]", text, "white", "red");
   }
+
   /**
-   * 
-   * @param {*} text 
+   * @param {*} text
    * @param {number} mode 0 -> only draw log, 1 -> draw log and start timer, 2 -> end timer (default: 0)
-   * @returns 
+   * @returns
    */
   debug(text, mode = 0) {
     switch (mode) {
@@ -129,9 +136,11 @@ class Logger {
         return;
     }
   }
+
   request(text) {
     this.log("[REQUEST]", text, "cyan", "black");
   }
+
   throwError(message, whereOccured, additionalInfo = "") {
     throw new Error(message + "\r\n" + whereOccured + (additionalInfo != "" ? `\r\n${additionalInfo}` : ""));
   }
@@ -139,14 +148,12 @@ class Logger {
 
 class LoggerUtils {
 
-  static getIsoDateString = (useFormatted = false) => {
-    if (useFormatted) {
-      return new Date().toISOString().
-        replace(/T/, ' ').
-        replace(/\..+/, '');
-    }
-    return new Date().toISOString();
+  static getIsoDateString(){
+    return new Date().toISOString().
+      replace(/T/, ' ').
+      replace(/\..+/, '');
   }
+
 }
 
 module.exports = new Logger();
