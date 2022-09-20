2bf6a63de2468486a71643d07cd324e4946ea811
ae_King <aclengen@gmail.com>
2022-09-20 10:43:45 -0400

    rewards being moved to inventory

--- routes/client/Game.js
+++ routes/client/Game.js
@@ -1,5 +1,6 @@
 const { GameController } = require("../../lib/controllers");
 const { Bot } = require("../../lib/models/Bot");
+const { Profile } = require("../../lib/models/Profile")
 const { FastifyResponse, writeFile, stringify } = require("../../utilities");
 
 module.exports = async function gameRoutes(app, _opts) {
@@ -22,14 +23,16 @@ module.exports = async function gameRoutes(app, _opts) {
 
     app.post('/client/game/bot/generate', async (request, reply) => {
         const bots = await Bot.generateBots(request, reply);
-        writeFile("./generatedBots.json", stringify(bots));
+        //writeFile("./generatedBots.json", stringify(bots));
         return FastifyResponse.zlibJsonReply(
             reply,
             FastifyResponse.applyBody(bots)
         );
     });
 
-    app.post("/client/game/logout", async (_request, reply) => {
+    app.post("/client/game/logout", async (request, reply) => {
+        const playerProfile = await Profile.get(await FastifyResponse.getSessionID(request));
+        await playerProfile.save();
         return FastifyResponse.zlibJsonReply(
             reply,
             FastifyResponse.applyBody({ status: "ok" })
