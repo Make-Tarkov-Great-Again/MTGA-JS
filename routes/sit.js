0f7c840373a1a0ac385b31126dec979b29453ea6
ae_King <aclengen@gmail.com>
2022-09-22 09:06:47 -0400

    remove SIT routes

--- routes/sit.js
+++
@@ -1,9 +0,0 @@
-const { logger, Response } = require("../utilities");
-
-module.exports = async function sitRoutes(app, _opts) {
-
-  app.post(`/client/sit-validator`, async (_request, reply) => {
-    return Response.zlibJsonReply(reply, true);
-  });
-
-}
