282bb378cd736097acdb17bb41cd520e27a165cc
king <aclengen@gmail.com>
2023-01-03 19:58:05 -0500

    download attempt

--- app.js
+++ app.js
@@ -27,30 +27,3 @@ module.exports = {
     webinterface,
     tasker
 };
-
-/* app.removeContentTypeParser("application/json");
-app.addContentTypeParser('application/json', function (req, body, done) {
-    try {
-        zlib.inflate(body, function (err, buffer) {
-            if (err && buffer === undefined) {
-                err.statusCode = 404;
-                logger.error(`Buffer is undefined`);
-                done(err, false);
-                return;
-            }
-            const inflatedString = buffer.toString('utf8');
-            if (inflatedString.length > 0) {
-                const data = parse(inflatedString);
-                done(null, data);
-                return;
-            } else {
-                done(null, false);
-                return;
-            }
-        });
-    } catch (error) {
-        err.statusCode = 404;
-        done(err, undefined);
-        return;
-    }
-}); */
