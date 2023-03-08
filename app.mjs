b735c2b6c5c6bb450388276c230079f50334f8fe
king <aclengen@gmail.com>
2023-03-08 07:32:10 -0500

    hehehehehehehehe LETS BREAK SHIT

---
+++ app.mjs
@@ -0,0 +1,17 @@
+import webinterface from "./lib/engine/WebInterface.mjs";
+import Server from './lib/engine/Server.mjs';
+
+await Promise.allSettled([
+    await Server.setServerConfig(),
+    await Server.registerCertificate(),
+    await Server.setFastify(),
+    await Server.printLogo(),
+    await Server.registerPlugins(),
+    await Server.setContentTypeParser(),
+    await Server.initializeServer()
+]);
+
+const app = Server.app;
+const database = Server.database;
+
+export { webinterface, app, database };
