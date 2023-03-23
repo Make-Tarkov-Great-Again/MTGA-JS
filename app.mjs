197ccc9d5c05d04d58f4a534874151c2b459cda0
Kestrel Kosdlic <70953258+EFHDev@users.noreply.github.com>
2023-03-23 16:16:12 -0400

    Update app.mjs to be a function
    This can be called globally with import "StartServer" from app.mjs, Why you would need it for anything besides the auto updater, dont ask me!!!!

--- app.mjs
+++ app.mjs
@@ -1,18 +1,21 @@
 import webinterface from "./lib/engine/WebInterface.mjs";
 import Server from './lib/engine/Server.mjs';
+import { checkForUpdates } from "./lib/ext/update.mjs";
 
-await Promise.allSettled([
-    await Server.setServerConfig(),
-    await Server.registerCertificate(),
-    await Server.setFastify(),
-    await Server.printLogo(),
-    await Server.registerPlugins(),
-    await Server.setContentTypeParser(),
-    await Server.initializeServer(),
-]);
+await checkForUpdates();
+export async function startServer() {
+    await Promise.allSettled([
+      Server.setServerConfig(),
+      Server.registerCertificate(),
+      Server.setFastify(),
+      Server.printLogo(),
+      Server.registerPlugins(),
+      Server.setContentTypeParser(),
+      Server.initializeServer(),
+    ]);
+  }
+  const app = Server.app;
+  const database = Server.database;
+  const rpc = Server.rpc;
 
-const app = Server.app;
-const database = Server.database;
-const rpc = Server.rpc;
-
-export { webinterface, app, database, rpc };
+  export { webinterface, app, database, rpc };
